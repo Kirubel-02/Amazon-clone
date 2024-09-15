f1d4b3c1fb7919e522845289d79fe073c80b9a56
Kirubel-02 <157580674+Kirubel-02@users.noreply.github.com>
2024-09-15 04:51:38 -0700

    carousel commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import CarouselEffect from "./components/Carousel/CarouselEffect";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
+      <CarouselEffect />
     </>
   );
 }
