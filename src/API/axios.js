45c8b27c47d477d9034851aae50fd971349a1263
Kirubel-02 <157580674+Kirubel-02@users.noreply.github.com>
2024-09-27 15:33:01 -0700

    deploy on netlify

--- src/API/axios.js
+++ src/API/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 // Set up the axios instance with baseURL for local development
 const axiosInstance = axios.create({
   // Make sure this URL matches the deployed or local Firebase function URL
-  baseURL: "http://127.0.0.1:5001/clone-bb050/us-central1/api",
-  // Alternatively, if deploying, set this to your live API URL:
-  // baseURL: "https://your-live-backend-url.com/api",
+  // baseURL: "http://127.0.0.1:5001/clone-bb050/us-central1/api",
+  // Alternatively, if deploying, set this to your live API URL by render:
+  baseURL: "https://amazon-api-hwai.onrender.com",
 
   // Optional timeout and headers can be adjusted based on requirements
   timeout: 10000, // Timeout after 10 seconds
